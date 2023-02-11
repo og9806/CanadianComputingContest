28b05bc589a45f1bc696a48c25a68b2744238044
og9806 <brian.chengshu.kang@gmail.com>
2023-02-11 14:34:33 -0500

    Worked on the problem quadrant selection

---
+++ src/year2017/QuadrantSelection.java
@@ -0,0 +1,9 @@
+package year2017;
+
+public class QuadrantSelection {
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+        //TODO: work on the solution
+    }
+}
